f010326cbfebe0680fb5a89399ae978a08a2109c
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2024-07-02 19:42:46 +0900

    CALLBACKの追加

--- WinApp.cpp
+++ WinApp.cpp
@@ -1,7 +1,7 @@
 #include "WinApp.h"
 
 
-LRESULT WinApp::WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+LRESULT CALLBACK WinApp::WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 {
 	{
 		if (ImGui_ImplWin32_WndProcHandler(hwnd, msg, wparam, lparam)) {
