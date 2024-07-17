709592db50b2669e78f3f034a571253125d8cebe
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2024-07-17 11:37:21 +0900

    追加

--- Sprite.h
+++ Sprite.h
@@ -1,10 +1,18 @@
 #pragma once
+
+class SpriteCommon;
 class Sprite
 {
 public:
 	/// <summary>
-    /// 初期化
-    /// </summary>
-	void Initialize();
+	/// 初期化
+	/// </summary>
+	void Initialize(SpriteCommon* spriteCommon);
+
+
+private: // メンバ関数
+
+	SpriteCommon* spriteCommon_ = nullptr;
+
 };
 
