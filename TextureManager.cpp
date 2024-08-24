e235c1578fc5fa0c5fd3116ee1837ff359ec21a5
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2024-08-24 21:09:42 +0900

    テクスチャデータ構造体の作成

--- TextureManager.cpp
+++ TextureManager.cpp
@@ -1,2 +1,17 @@
 #include "TextureManager.h"
 TextureManager* TextureManager::instance = nullptr;
+
+TextureManager* TextureManager::GetInstance()
+{
+	if (instance == nullptr) {
+		instance = new TextureManager;
+	}
+	return instance;
+
+}
+
+void TextureManager::Finalize()
+{
+	delete instance;
+	instance = nullptr;
+}
--- TextureManager.h
+++ TextureManager.h
@@ -1,10 +1,27 @@
 #pragma once
+#include <string>
+#include<wrl.h>
+#include "d3d12.h"
+#include "externals/DirectXTex/DirectXTex.h"
 
 // テクスチャマネージャー
 class TextureManager
 {
-public:
+private:
+	// テクスチャ1枚分のデータ
+	struct TextureData {
+		std::string filePath;
+		DirectX::TexMetadata metadata;
+		Microsoft::WRL::ComPtr<ID3D12Resource> resource;
+		D3D12_CPU_DESCRIPTOR_HANDLE srvHandleCPU;
+		D3D12_GPU_DESCRIPTOR_HANDLE srvHandleGPU;
+	};
 
+public:
+	// シングルトンインスタンスの取得
+	static TextureManager* GetInstance();
+	// 終了
+	void Finalize();
 
 
 
@@ -12,7 +29,6 @@ public:
 
 private:
 	static TextureManager* instance;
-
 	TextureManager() = default;
 	~TextureManager() = default;
 	TextureManager(TextureManager&) = delete;
