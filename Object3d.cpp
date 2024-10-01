28a66d988fbb3cde0a386fb7c2a9d65595e259fd
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2024-10-01 11:42:40 +0900

    実行を確認

--- Object3d.cpp
+++ Object3d.cpp
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <fstream>
 #include <sstream>
+#include "TextureManager.h"
 
 void Object3d::Initialize(Object3dCommon* object3dCommon)
 {
@@ -10,9 +11,161 @@ void Object3d::Initialize(Object3dCommon* object3dCommon)
 	this->object3dCommon_ = object3dCommon;
 	// モデル読み込み
 	modelData_ = LoadObjFile("Resources","plane.obj");
+
+	//VertexResource();
+
+	//IndexResource();
+
+	//MaterialResource();
+
+	//TextureManager::GetInstance()->LoadTexture(textureFilePath);
+
+	//textureIndex = TextureManager::GetInstance()->GetTextureIndexByFilePath(textureFilePath);
+
+	//AdjustTaxtureSize();
 }
 
-MaterialData Object3d::LoadMaterialTemplateFile(const std::string& directoryPath, const std::string& filename)
+void Object3d::VertexResource()
+{
+	// リソース
+	vertexResource_ = object3dCommon_->GetDxCommon()->CreateBufferResource(sizeof(VertexData) * 6);
+	// リソースの先頭アドレスから使う
+	vertexBufferView_.BufferLocation = vertexResource_->GetGPUVirtualAddress();
+	// 使用するリソースサイズは頂点3つ分のサイズ
+	vertexBufferView_.SizeInBytes = sizeof(VertexData) * 6;
+	// 1頂点あたりのサイズ
+	vertexBufferView_.StrideInBytes = sizeof(VertexData);
+
+}
+
+void Object3d::CreateVertex()
+{
+	VertexData* vertexData = nullptr;
+	vertexResource_->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
+	/*=====================================================//
+							 index有
+	=======================================================*/
+
+	// アンカーポイント
+	float left = 0.0f - anchorPoint_.x;
+	float right = 1.0f - anchorPoint_.x;
+	float top = 0.0f - anchorPoint_.y;
+	float bottom = 1.0f - anchorPoint_.y;
+
+	// 左右反転
+	if (isFlipX_) {
+		left = -left;
+		right = -right;
+	}
+	//上下反転
+	if (isFlipY_) {
+		top = -top;
+		bottom = -bottom;
+	}
+
+	const DirectX::TexMetadata& metadata = TextureManager::GetInstance()->GetMetaData(textureIndex);
+	float tex_left = textureLeftTop_.x / metadata.width;
+	float tex_right = (textureLeftTop_.x + textureSize_.x) / metadata.width;
+	float tex_top = textureLeftTop_.y / metadata.height;
+	float tex_bottom = (textureLeftTop_.y + textureSize_.y) / metadata.height;
+	// 左下
+	vertexData[0].position = { left, bottom, 0.0f, 1.0f };
+	vertexData[0].texcoord = { tex_left, tex_bottom };
+
+	// 左上
+	vertexData[1].position = { left, top, 0.0f, 1.0f };
+	vertexData[1].texcoord = { tex_left, tex_top };
+
+	// 右下
+	vertexData[2].position = { right, bottom, 0.0f, 1.0f };
+	vertexData[2].texcoord = { tex_right, tex_bottom };
+
+	// 右上
+	vertexData[3].position = { right, top, 0.0f, 1.0f };
+	vertexData[3].texcoord = { tex_right, tex_top };
+
+	// 2枚目の三角形用
+	vertexData[4].position = { left, top, 0.0f, 1.0f };  // 左上
+	vertexData[4].texcoord = { tex_left, tex_top };
+
+	vertexData[5].position = { right, bottom, 0.0f, 1.0f };  // 右下
+	vertexData[5].texcoord = { tex_right, tex_bottom };
+
+	vertexData[0].normal = { 0.0f,0.0f,-1.0f };
+	vertexData[1].normal = { 0.0f,0.0f,-1.0f };
+	vertexData[2].normal = { 0.0f,0.0f,-1.0f };
+	vertexData[3].normal = { 0.0f,0.0f,-1.0f };
+	vertexData[4].normal = { 0.0f,0.0f,-1.0f };
+	vertexData[5].normal = { 0.0f,0.0f,-1.0f };
+}
+
+void Object3d::IndexResource()
+{
+	// リソース
+	indexResource_ = object3dCommon_->GetDxCommon()->CreateBufferResource(sizeof(uint32_t) * 6);
+	// リソースの先頭アドレスから使う
+	indexBufferView_.BufferLocation = indexResource_->GetGPUVirtualAddress();
+	// 使用するリソースサイズはもとの頂点のサイズ
+	indexBufferView_.SizeInBytes = sizeof(uint32_t) * 6;
+	// インデックスはuint32_tとする
+	indexBufferView_.Format = DXGI_FORMAT_R32_UINT;
+
+	CreateIndex();
+}
+
+void Object3d::CreateIndex()
+{
+	uint32_t* indexData = nullptr;
+	indexResource_->Map(0, nullptr, reinterpret_cast<void**>(&indexData));
+	indexData[0] = 0;  // 最初の三角形
+	indexData[1] = 1;
+	indexData[2] = 2;
+	indexData[3] = 1;  // 2つ目の三角形
+	indexData[4] = 3;
+	indexData[5] = 2;
+}
+
+void Object3d::MaterialResource()
+{
+	// リソース作成
+	materialResource_ = object3dCommon_->GetDxCommon()->CreateBufferResource(sizeof(Material));
+	// データを書き込むためのアドレスを取得して割り当て
+	materialResource_->Map(0, nullptr, reinterpret_cast<void**>(&materialData_));
+	// マテリアルデータの初期化
+	materialData_->color = { 1.0f,1.0f, 1.0f, 1.0f };
+	materialData_->enableLighting = false;
+	materialData_->uvTransform = MakeIdentity4x4();
+
+
+	TransformResource();
+}
+
+void Object3d::TransformResource()
+{
+	// リソース作成
+	transformationMatrixResource_ = object3dCommon_->GetDxCommon()->CreateBufferResource(sizeof(TransformationMatrix));
+	// データを書き込むためのアドレスを取得して割り当て
+	transformationMatrixResource_->Map(0, nullptr, reinterpret_cast<void**>(&transformationMatrixData_));
+
+	// 単位行列を書き込む
+	transformationMatrixData_->WVP = MakeIdentity4x4();
+	transformationMatrixData_->World = MakeIdentity4x4();
+
+}
+
+void Object3d::AdjustTaxtureSize()
+{
+	// テクスチャメタデータを取得
+	const DirectX::TexMetadata& metadata = TextureManager::GetInstance()->GetMetaData(textureIndex);
+
+	textureSize_.x = static_cast<float>(metadata.width);
+	textureSize_.y = static_cast<float>(metadata.height);
+	// 画像サイズをテクスチャサイズに合わせる
+	size_ = textureSize_;
+
+}
+
+Object3d::MaterialData Object3d::LoadMaterialTemplateFile(const std::string& directoryPath, const std::string& filename)
 {
 	MaterialData materialData;
 	std::string line;
@@ -34,7 +187,7 @@ MaterialData Object3d::LoadMaterialTemplateFile(const std::string& directoryPath
 	return materialData;
 }
 
-ModelData Object3d::LoadObjFile(const std::string& directoryPath, const std::string& filename)
+Object3d::ModelData Object3d::LoadObjFile(const std::string& directoryPath, const std::string& filename)
 {
 	// 1. 必要な変数宣言
 	ModelData modelData;		   // 構築するModelData
--- Object3d.h
+++ Object3d.h
@@ -1,37 +1,87 @@
 #pragma once
+#include <wrl.h>
+#include <d3d12.h>
 #include <string>
 #include <vector>
 #include "math/Vector4.h"
 #include "math/Matrix4x4.h"
 #include "math/Vector2.h"
+#include "TransformationMatrix.h"
 
 //　構造体
-struct VertexData {
-	Vector4 position;
-	Vector2 texcoord;
-	Vector3 normal;
-};
 
-struct MaterialData {
-	std::string textureFilePath;
-};
-
-struct ModelData {
-	std::vector<VertexData> vertices;
-	MaterialData material;
-};
 //前方宣言
 class Object3dCommon;
 // 3Dオブジェクト
 class Object3d
 {
+public: // 構造体
+	struct VertexData {
+		Vector4 position;
+		Vector2 texcoord;
+		Vector3 normal;
+	};
+
+
+	struct MaterialData {
+		std::string textureFilePath;
+	};
+
+	struct ModelData {
+		std::vector<VertexData> vertices;
+		MaterialData material;
+	};
+
+	struct Material {
+		Vector4 color;
+		int32_t enableLighting;
+		float padding[3];
+		Matrix4x4 uvTransform;
+	};
+
 public: // メンバ関数
 	/// <summary>
 	/// 初期化
 	/// </summary>
 	void Initialize(Object3dCommon* object3dCommon);
 
+	
 private:
+	/// <summary>
+	/// 頂点リソース
+	/// </summary>
+	void VertexResource();
+
+	/// <summary>
+	/// 頂点
+	/// </summary>
+	void CreateVertex();
+
+	/// <summary>
+	/// インデックスリソース
+	/// </summary>
+	void IndexResource();
+
+	/// <summary>
+	/// 頂点インデックス
+	/// </summary>
+	void CreateIndex();
+
+	/// <summary>
+	/// マテリアルリソース
+	/// </summary>
+	void MaterialResource();
+
+	/// <summary>
+	/// 座標変換リソース
+	/// </summary>
+	void TransformResource();
+
+	/// <summary>
+	/// テクスチャサイズをイメージに合わせる
+	/// </summary>
+	void AdjustTaxtureSize();
+
 	// .mtlファイルの読み取り
 	static MaterialData LoadMaterialTemplateFile(const std::string& directoryPath, const std::string& filename);
 	//.objファイルの読み取り
@@ -42,5 +92,46 @@ private: // メンバ変数
 	Object3dCommon* object3dCommon_ = nullptr;
 	// objファイルのデータ
 	ModelData modelData_;
+
+	// バッファリソース
+	Microsoft::WRL::ComPtr<ID3D12Resource> vertexResource_;
+	// インデックス
+	Microsoft::WRL::ComPtr<ID3D12Resource> indexResource_;
+	D3D12_INDEX_BUFFER_VIEW indexBufferView_{};
+	// マテリアル
+	Microsoft::WRL::ComPtr<ID3D12Resource> materialResource_;
+	Material* materialData_ = nullptr;
+	// 座標変換
+	Microsoft::WRL::ComPtr<ID3D12Resource> transformationMatrixResource_;
+	TransformationMatrix* transformationMatrixData_ = nullptr;
+
+	// バッファリソース内のデータを指すポインタ
+	VertexData* vertexData_ = nullptr;
+	// バッファリソースの使い道を補足するバッファービュー
+	D3D12_VERTEX_BUFFER_VIEW vertexBufferView_{};
+
+
+	// テクスチャ番号
+	uint32_t textureIndex = 0;
+
+	// テクスチャ左上座標
+	Vector2 textureLeftTop_ = { 0.0f,0.0f };
+	// テクスチャ切り出しサイズ
+	Vector2 textureSize_ = { 64.0f,64.0f };
+
+	// スプライト
+	VertexData* vertexData = nullptr;
+	Vector2 position_ = { 0.0f,0.0f };
+	float rotation_ = 0.0f;
+	Vector2 size_ = { 100.0f,100.0f };
+
+	// アンカーポイント
+	Vector2 anchorPoint_ = { 0.0f,0.0f };
+
+	// 左右フリップ
+	bool isFlipX_ = false;
+	// 上下フリップ
+	bool isFlipY_ = false;
+
 };
 
