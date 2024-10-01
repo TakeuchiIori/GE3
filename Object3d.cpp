7b8c741dd731ca0692b319b9d8f5bdcd111d26ff
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2024-10-01 10:58:15 +0900

    objファイル構造体の作成

--- Object3d.cpp
+++ Object3d.cpp
@@ -1,6 +1,9 @@
 #include "Object3d.h"
+#include "Object3dCommon.h"
 
-void Object3d::Initialize()
+void Object3d::Initialize(Object3dCommon* object3dCommon)
 {
+	// 引数で受け取ってメンバ変数に記録する
+	this->object3dCommon_ = object3dCommon;
 
 }
--- Object3d.h
+++ Object3d.h
@@ -1,14 +1,31 @@
 #pragma once
+#include <string>
+//前方宣言
+class Object3dCommon;
 // 3Dオブジェクト
-
 class Object3d
 {
+public: //　構造体
+	struct MaterialData {
+		std::string textureFilePath;
+	};
+
+	struct ModelData {
+		std::vector<VertexData> vertices;
+		MaterialData material;
+	};
+
 public: // メンバ関数
 	/// <summary>
 	/// 初期化
 	/// </summary>
-	void Initialize();
+	void Initialize(Object3dCommon* object3dCommon);
+
+private:
 
 
+
+private: // メンバ変数
+	Object3dCommon* object3dCommon_ = nullptr;
 };
 
