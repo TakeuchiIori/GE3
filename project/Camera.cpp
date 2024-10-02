a8cb9c4b811b3d8f2571cbac6f031a6bb8529f76
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2024-10-02 14:57:51 +0900

    デフォルトコンストラクタまで

--- project/Camera.cpp
+++ project/Camera.cpp
@@ -1,9 +1,28 @@
 #include "Camera.h"
 #include "MathFunc.h"
+#include "WinApp.h"
+Camera::Camera()
+    : transform_({ {1.0f, 1.0f, 1.0f}, {0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f} })
+    , fovY_(0.45f)
+    , aspectRatio_(float(WinApp::kClientWidth) / float(WinApp::kClientHeight))
+    , nearClip_(0.1f)
+    , farClip_(100.0f)
+    , worldMatrix_(MakeAffineMatrix(transform_.scale, transform_.rotate, transform_.translate))
+    , viewMatrix_(Inverse(worldMatrix_))
+    , projectionMatrix_(MakePerspectiveFovMatrix(fovY_, aspectRatio_, nearClip_, farClip_))
+    , viewProjectionMatrix_(Multiply(viewMatrix_, projectionMatrix_))
+{}
+
 void Camera::Update()
 {
 	// transformからアフィン変換行列を計算
 	worldMatrix_ = MakeAffineMatrix(transform_.scale, transform_.rotate, transform_.translate);
 	// worldMatrixの逆行列
 	viewMatrix_ = Inverse(worldMatrix_);
+	// プロジェクション行列の更新
+	projectionMatrix_ = MakePerspectiveFovMatrix(fovY_, aspectRatio_, nearClip_,farClip_);
+	// 合成行列
+	viewProjectionMatrix_ = Multiply(viewMatrix_, projectionMatrix_);
+
+
 }
--- project/Camera.h
+++ project/Camera.h
@@ -4,7 +4,10 @@
 class Camera
 {
 public: // メンバ関数
-
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	Camera();
 
 	/// <summary>
 	/// 更新
@@ -12,11 +15,34 @@ public: // メンバ関数
 	void Update();
 
 
+public: // アクセッサ
+    // Setter
+    void SetRotate(const Vector3& rotate) {transform_.rotate = rotate;}
+    void SetTranslate(const Vector3& translate) {transform_.translate = translate; }
+	void SetFovY(float fovY) { fovY_ = fovY; }
+    void SetAspectRatio(float aspectRatio) { aspectRatio_ = aspectRatio;}
+	void SetNearClip(float nearClip) { nearClip_ = nearClip; }
+    void SetFarClip(float farClip) {farClip_ = farClip;     }
 
+	// getter
+	const Matrix4x4& GetWorldMatrix() const { worldMatrix_; }
+	const Matrix4x4& GetViewMatrix() const { viewMatrix_; }
+	const Matrix4x4& GetProjectionMatrix() const { projectionMatrix_; }
+	const Matrix4x4& GetViewProjectionMatrix() const { viewProjectionMatrix_; }
+	Vector3 GetRotate() const { transform_.rotate; }
+	Vector3 GetTranslate() const { transform_.translate; }
+	Vector3 GetScale() const { transform_.scale; }
 
 private: // メンバ変数
-	Transform transform_;
-	Matrix4x4 worldMatrix_;
-	Matrix4x4 viewMatrix_;
+	Transform transform_;	   
+	Matrix4x4 worldMatrix_;	   
+	Matrix4x4 viewMatrix_;	   
+	Matrix4x4 projectionMatrix_;  // 投影行列
+	Matrix4x4 viewProjectionMatrix_;
+	float fovY_;        // 水平方向視野角 (Field of View)
+	float aspectRatio_;          // アスペクト比
+	float nearClip_;			 // ニアクリップ距離
+	float farClip_;				 // ファークリップ距離
+
 };
 
