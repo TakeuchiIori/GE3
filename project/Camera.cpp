95d5d8d48aec4beba234b135d2e8024ad71dfcde
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2024-10-02 14:17:40 +0900

    transformからアフィン変換行列を計算 worldMatrixの逆行列

--- project/Camera.cpp
+++ project/Camera.cpp
@@ -1,1 +1,9 @@
 #include "Camera.h"
+#include "MathFunc.h"
+void Camera::Update()
+{
+	// transformからアフィン変換行列を計算
+	worldMatrix_ = MakeAffineMatrix(transform_.scale, transform_.rotate, transform_.translate);
+	// worldMatrixの逆行列
+	viewMatrix_ = Inverse(worldMatrix_);
+}
--- project/Camera.h
+++ project/Camera.h
@@ -3,13 +3,20 @@
 #include "Matrix4x4.h"
 class Camera
 {
-public:
+public: // メンバ関数
+
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
 
 
 
 
 private: // メンバ変数
-
-
+	Transform transform_;
+	Matrix4x4 worldMatrix_;
+	Matrix4x4 viewMatrix_;
 };
 
