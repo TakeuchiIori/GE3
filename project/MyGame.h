8ee98320baed3610ec8dff0bec24acde3a108ffb
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2024-10-06 10:51:24 +0900

    クラス作成

---
+++ project/MyGame.h
@@ -0,0 +1,29 @@
+#pragma once
+class MyGame
+{
+
+public: // メンバ関数
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 終了
+	/// </summary>
+	void Finalize();
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+private: // メンバ変数
+};
+
