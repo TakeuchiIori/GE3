3cf03493f9d5c634a32b19e76650a879401187ff
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2024-10-06 11:44:09 +0900

    基底クラスの宣言

--- project/application/base/Framework.h
+++ project/application/base/Framework.h
@@ -1,5 +1,42 @@
 #pragma once
+
+// ゲーム全体
 class Framework
 {
+public: // メンバ関数
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	virtual void Initialize();
+
+	/// <summary>
+	/// 終了
+	/// </summary>
+	virtual void Finalize();
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	virtual void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	virtual void Draw() = 0;
+
+	/// <summary>
+	/// 終了フラグのチェック
+	/// </summary>
+	/// <returns></returns>
+	virtual bool isEndRequst() { return endRequst_; }
+
+	// 呼び出さないとリークするぞ
+	virtual ~Framework() = default;
+
+private:
+	// ゲーム終了フラグ
+	bool endRequst_ = false;
+
 };
 
