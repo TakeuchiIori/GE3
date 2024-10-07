52a4b6ca81b2e59acb50296563cf64c06b399e98
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2024-10-07 10:00:59 +0900

    クラス追加

---
+++ project/GameScene.h
@@ -0,0 +1,25 @@
+#pragma once
+class GameScene
+{
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 終了
+	/// </summary>
+	void Finalize();
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+};
+
