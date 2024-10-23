18b90c96c46fa54ac9ef131747f5eb7b8e4fa5d0
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2024-10-23 12:01:20 +0900

    見やすく関数分け＋神視点追加

--- project/application/scene/GameScene.cpp
+++ project/application/scene/GameScene.cpp
@@ -8,57 +8,108 @@
 
 void GameScene::Initialize()
 {
-	///============ モデル読み込み ============///
-	ModelManager::GetInstance()->LoadModel("float_body.obj");
-
-	///============ カメラ初期化 ============///
-	camera_ = std::make_unique<Camera>();
-
-	Object3dCommon::GetInstance()->SetDefaultCamera(camera_.get());
-
-	///============ オブジェクト初期化 ============///
-	player_ = std::make_unique<Player>();
-	player_->Initailize();
-
-	test_ = std::make_unique<Object3d>();
-	test_->Initialize(Object3dCommon::GetInstance());
-	test_->SetModel("float_body.obj");
-	testWorldTransform_.Initialize();
+    LoadModels();
+    InitializeCamera();
+    InitializeObjects();
+    // 初期カメラモード設定
+    cameraMode_ = CameraMode::FOLLOW;
 }
 
 void GameScene::Finalize()
-{ 
-	// シーン専用のリソース解放処理
-
+{
+    FinalizeCamera();
 }
 
 void GameScene::Update()
 {
-	// プレイヤーの更新
-	player_->Update();
-	
-	// プレイヤーの位置を取得
-	Vector3 playerPos = player_->GetPosition();
-	camera_->FollowCamera(playerPos);
-
-	testWorldTransform_.TransferMatrix();
-	// カメラの更新
-	camera_->Update();
-	
+    UpdatePlayer();
+    UpdateCameraMode();
+    UpdateCamera();
+    testWorldTransform_.TransferMatrix();
+    cameraManager_.UpdateAllCameras();
 }
-
-
 
 void GameScene::Draw()
 {
-	///======================  描画準備  ========================///
-	
-	/// 3Dオブジェクトの描画準備
-	Object3dCommon::GetInstance()->DrawPreference();
-	/// 2DSpriteの描画準備
-	SpriteCommon::Getinstance()->DrawPreference();
+    PrepareDraw();
+    DrawObjects();
+}
 
-	///======================  実際に描画  ========================///
-	player_->Draw();
-	test_->Draw(testWorldTransform_);
+void GameScene::LoadModels()
+{
+    ModelManager::GetInstance()->LoadModel("float_body.obj");
+}
+
+void GameScene::InitializeCamera()
+{
+    currentCamera_ = cameraManager_.AddCamera();
+    Object3dCommon::GetInstance()->SetDefaultCamera(currentCamera_.get());
+}
+
+void GameScene::FinalizeCamera()
+{
+    cameraManager_.RemoveCamera(currentCamera_);
+}
+
+void GameScene::InitializeObjects()
+{
+    player_ = std::make_unique<Player>();
+    player_->Initailize();
+
+    test_ = std::make_unique<Object3d>();
+    test_->Initialize();
+    test_->SetModel("float_body.obj");
+    testWorldTransform_.Initialize();
+}
+
+void GameScene::UpdatePlayer()
+{
+    player_->Update();
+}
+
+void GameScene::UpdateCameraMode()
+{
+#ifdef _DEBUG
+    ImGui::Begin("Camera Mode");
+    if (ImGui::Button("Follow Camera")) {
+        cameraMode_ = CameraMode::FOLLOW;
+    }
+    if (ImGui::Button("Top-Down Camera")) {
+        cameraMode_ = CameraMode::TOP_DOWN;
+    }
+    ImGui::End();
+#endif
+}
+
+void GameScene::UpdateCamera()
+{
+    switch (cameraMode_)
+    {
+    case CameraMode::FOLLOW:
+    {
+        Vector3 playerPos = player_->GetPosition();
+        currentCamera_->FollowCamera(playerPos);
+    }
+    break;
+    case CameraMode::TOP_DOWN:
+    {
+        Vector3 topDownPosition = Vector3(0.0f, 100.0f, 0.0f);
+        currentCamera_->SetTopDownCamera(topDownPosition);
+    }
+    break;
+    default:
+        break;
+    }
+}
+
+void GameScene::PrepareDraw()
+{
+    Object3dCommon::GetInstance()->DrawPreference();
+    SpriteCommon::Getinstance()->DrawPreference();
+}
+
+void GameScene::DrawObjects()
+{
+    player_->Draw();
+    test_->Draw(testWorldTransform_);
 }
