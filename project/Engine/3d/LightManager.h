1e33992c86d678198cb44af2a274529d5f5afec5
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2024-12-04 10:44:14 +0900

    コミット

---
+++ project/Engine/3d/LightManager.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <wrl.h>
+#include <d3d12.h>
+#include <string>
+#include <vector>
+#include "Vector4.h"
+#include "Matrix4x4.h"
+#include "Vector2.h"
+#include "Vector3.h"
+
+class LightManager
+{
+};
+
