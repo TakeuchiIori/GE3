f47424d6db5d7566011b11bda8f5bef5464e2e9b
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2024-12-09 22:55:25 +0900

    構造体

--- project/Engine/model/Model.h
+++ project/Engine/model/Model.h
@@ -9,6 +9,7 @@
 #include "Vector3.h"
 #include "DirectXCommon.h"
 #include "WorldTransform.h"
+#include <optional>
 
 // assimp
 #include <assimp/scene.h>
@@ -71,22 +72,37 @@ private: // 構造体
 		tValue value;
 	};
 
-	//using KeyframeVector3 = Keyframe<Vector3>;
-	//using KeyframeQuaternion = Keyframe<Quaternion>;
-
 	template<typename tValue>
 	struct AnimationCurve {
 		std::vector<Keyframe<tValue>> keyframes;
 	};
+
 	struct NodeAnimation {
 		std::vector<KeyframeVector3> translate;
 		std::vector<KeyframeQuaternion> rotate;
 		std::vector<KeyframeVector3> scale;
 	};
+
 	struct Animation {
 		float duration; // アニメーション全体の尺（秒）
 		// NodeAnimationの集合。Node名で開けるように
 		std::map<std::string, NodeAnimation> nodeAnimations;
+	};
+
+	struct Joint {
+		QuaternionTransform transform; // Transform情報
+		Matrix4x4 localMatrix; // localMatrix
+		Matrix4x4 skeletonSpaceMatrix; // skeletonSpaceでの変換行列
+		std::string name; // 名前
+		std::vector<int32_t> children; // 子JointのIndexのリスト。いなければ空
+		int32_t index; // 自身のIndex
+		std::optional<int32_t> parent; // 親JointのIndex。いなければnull
+	};
+
+	struct Skeleton {
+		int32_t root; // RootJointのIndex
+		std::map<std::string, int32_t> jointMap; // Joint名とIndexとの辞書
+		std::vector<Joint> joints; // 所属しているジョイント
 	};
 
 public: // メンバ関数
