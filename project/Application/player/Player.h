7edb0ae620cc5428e276eb49db906981f5dcc508
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2024-12-12 21:31:29 +0900

    削除したファイルが復活

---
+++ project/Application/player/Player.h
@@ -0,0 +1,90 @@
+#pragma once
+// Engine
+#include "3d/object3d/Object3d.h"
+#include "io/input/Input.h"
+#include "3d/WorldTransform.h"
+#include "3d/collider/Collider.h"
+
+// C++
+#include <memory>
+
+// Math
+#include "math/MathFunc.h"
+#include "math/Vector3.h" 
+
+class Player : public Collider
+{
+
+public: // メンバ関数（公開）
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+	/// <summary>
+	/// ImGui
+	/// </summary>
+	void ShowCoordinatesImGui();
+
+public: // ポリモーフィズム
+
+	/// <summary>
+	/// 衝突を検出したら呼び出されるコールバック関数
+	/// </summary>
+	void OnCollision([[maybe_unused]] Collider* other) override;
+
+	/// <summary>
+	/// 中心座標を取得
+	/// </summary>
+	/// <returns></returns>
+	Vector3 GetCenterPosition() const override;
+
+
+
+private: // メンバ関数（非公開）
+
+	/// <summary>
+	/// 更新の末端でやる処理
+	/// </summary>
+	void UpdateWorldTransform();
+
+	/// <summary>
+	/// 移動処理をまとめた関数
+	/// </summary>
+	void Move();
+
+	/// <summary>
+	/// キーボードで移動処理
+	/// </summary>
+	void MoveKey();
+
+
+public: // アクセッサ
+	// プレイヤーの位置を取得する関数
+	const Vector3& GetPosition() const { return worldTransform_.translation_; }
+
+	const Vector3& GetRotation() const { return worldTransform_.rotation_; }
+
+private: // メンバ変数
+
+	WorldTransform worldTransform_;
+	std::unique_ptr<Object3d> base_ = nullptr;
+	Input* input_ = nullptr;
+
+	bool isColliding_ = false;
+	Vector3 moveSpeed_;
+	bool isDrawEnabled_ = true;
+
+};
+
---
+++ project/Application/scene/GameScene.cpp
@@ -0,0 +1,211 @@
+#include "GameScene.h"
+// Engine
+#include "3d/SceneManager.h"
+#include "io/input/Input.h"
+#include "bases/TextureManager.h"
+#include "io/particle/ParticleManager.h"
+
+
+#ifdef _DEBUG
+#include "imgui.h"
+#endif // DEBUG
+
+/// <summary>
+/// 初期化処理
+/// </summary>
+void GameScene::Initialize()
+{
+    // カメラの生成
+    currentCamera_ = cameraManager_.AddCamera();
+    Object3dCommon::GetInstance()->SetDefaultCamera(currentCamera_.get());
+   
+    // 線
+    line_ = std::make_unique<Line>();
+    line_->Initialize();
+    line_->SetCamera(currentCamera_.get());
+
+    // 各オブジェクトの初期化
+    player_ = std::make_unique<Player>();
+    player_->Initialize();
+
+    // test
+    test_ = std::make_unique<Object3d>();
+    test_->Initialize();
+    test_->SetModel("sneakWalk.gltf",true);
+    testWorldTransform_.Initialize();
+    //test_->SetLine(line_.get());
+
+    // 初期カメラモード設定
+    cameraMode_ = CameraMode::FOLLOW;
+
+    // パーティクル
+    std::string particleName = "Circle";
+    ParticleManager::GetInstance()->SetCamera(currentCamera_.get());
+    ParticleManager::GetInstance()->CreateParticleGroup(particleName, "Resources/images/circle.png");
+    emitterPosition_ = Vector3{ 0.0f, 0.0f, 0.0f }; // エミッタの初期位置
+    particleCount_ = 1;
+    particleEmitter_ = std::make_unique<ParticleEmitter>(particleName, emitterPosition_, particleCount_);
+
+   
+}
+
+/// <summary>
+/// 更新処理
+/// </summary>
+void GameScene::Update()
+{
+    
+    //if (Input::GetInstance()->TriggerKey(DIK_RETURN)) {
+    //    SceneManager::GetInstance()->ChangeScene("TITLE");
+    //}
+    
+    // プレイヤーの更新
+    player_->Update();
+    test_->UpdateAnimation();
+
+    // カメラ更新
+    UpdateCameraMode();
+    UpdateCamera();
+
+    // パーティクル更新
+    ParticleManager::GetInstance()->Update();
+    ShowImGui();
+    particleEmitter_->Update();
+   
+  
+
+    // ワールドトランスフォーム更新
+    testWorldTransform_.UpdateMatrix();
+    cameraManager_.UpdateAllCameras();
+
+    //=====================================================//
+    /*                  これより下は触るな危険　　　　　　　   　*/
+    //=====================================================//
+   
+    // ライティング
+    LightManager::GetInstance()->ShowLightingEditor();
+}
+
+
+/// <summary>
+/// 描画処理
+/// </summary>
+void GameScene::Draw()
+{
+#pragma region 2Dスプライト描画
+    SpriteCommon::GetInstance()->DrawPreference();
+    /// <summary>
+    /// ここから描画可能です
+    /// </summary>
+
+    ParticleManager::GetInstance()->Draw();
+
+
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+    Object3dCommon::GetInstance()->DrawPreference();
+    LightManager::GetInstance()->SetCommandList();
+    /// <summary>
+    /// ここから描画可能です
+    /// </summary>
+    
+    player_->Draw();
+    test_->Draw(testWorldTransform_);
+  
+    line_->DrawLine(start_, end_);
+
+    //test_->DrawSkeleton();
+
+#pragma endregion
+
+
+}
+
+/// <summary>
+/// 解放処理
+/// </summary>
+void GameScene::Finalize()
+{
+    cameraManager_.RemoveCamera(currentCamera_);
+}
+
+void GameScene::UpdateCameraMode()
+{
+#ifdef _DEBUG
+    ImGui::Begin("Camera Mode");
+    if (ImGui::Button("DEFAULT Camera")) {
+        cameraMode_ = CameraMode::DEFAULT;
+    }
+    if (ImGui::Button("Follow Camera")) {
+        cameraMode_ = CameraMode::FOLLOW;
+    }
+    if (ImGui::Button("Top-Down Camera")) {
+        cameraMode_ = CameraMode::TOP_DOWN;
+    }
+    if (ImGui::Button("FPS Camera")) {
+        cameraMode_ = CameraMode::FPS;
+    }
+    ImGui::End();
+#endif
+}
+
+void GameScene::UpdateCamera()
+{
+    switch (cameraMode_)
+    {
+    case CameraMode::DEFAULT:
+    {
+        currentCamera_->DefaultCamera();
+    }
+    break;
+    case CameraMode::FOLLOW:
+    {
+        Vector3 playerPos = player_->GetPosition();
+        currentCamera_->FollowCamera(playerPos);
+    }
+    break;
+    case CameraMode::TOP_DOWN:
+    {
+        Vector3 topDownPosition = Vector3(0.0f, 100.0f, 0.0f);
+        currentCamera_->SetTopDownCamera(topDownPosition + player_->GetPosition());
+    }
+    break;
+    case CameraMode::FPS:
+    {
+        Vector3 playerPos = player_->GetPosition();
+        currentCamera_->SetFPSCamera(playerPos, player_->GetRotation());
+    }
+    break;
+
+    default:
+        break;
+    }
+}
+
+void GameScene::ShowImGui()
+{
+#ifdef _DEBUG
+    ImGui::Begin("Emitter");
+    ImGui::DragFloat3("Emitter Position", &emitterPosition_.x, 0.1f);
+    particleEmitter_->SetPosition(emitterPosition_);
+
+    // パーティクル数の表示と調整
+    ImGui::Text("Particle Count: %.0d", particleCount_); // 現在のパーティクル数を表示
+    if (ImGui::Button("Increase Count")) {
+        particleCount_ += 1; // パーティクル数を増加
+    }
+    if (ImGui::Button("Decrease Count")) {
+        if (particleCount_ > 1) { // パーティクル数が1未満にならないように制限
+            particleCount_ -= 1;
+        }
+    }
+    particleEmitter_->SetCount(particleCount_);
+
+
+    ImGui::End();
+#endif // _DEBUG
+}
+
+
+
