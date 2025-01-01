3a13ad4835fa8506497d19c571cf95826cdded8c
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2025-01-01 19:17:55 +0900

    プレイヤーの武器クラスの作成

---
+++ project/Application/Objects/Player/PlayerWeapon/PlayerWeapon.cpp
@@ -0,0 +1,29 @@
+#include "PlayerWeapon.h"
+// Engine
+#include "Loaders./Model./ModelManager.h"
+#include "Object3D./Object3dCommon.h"
+
+#ifdef _DEBUG
+#include "imgui.h" 
+#endif // _DEBUG
+
+void PlayerWeapon::Initialize()
+{
+	// object3dの初期化
+	weapon_ = std::make_unique<Object3d>();
+	weapon_->Initialize();
+	weapon_->SetModel("");
+
+
+
+}
+
+void PlayerWeapon::Update()
+{
+	worldTransform_.UpdateMatrix();
+}
+
+void PlayerWeapon::Draw()
+{
+	weapon_->Draw(worldTransform_);
+}
---
+++ project/Application/Objects/Player/PlayerWeapon/PlayerWeapon.h
@@ -0,0 +1,53 @@
+#pragma once
+
+// Engine
+#include "Object3D./Object3d.h"
+#include "Systems/Input./Input.h"
+#include "WorldTransform./WorldTransform.h"
+#include "Collision./Collider.h"
+
+// C++
+#include <memory>
+
+// Math
+#include "MathFunc.h"
+#include "Vector3.h" 
+
+class PlayerWeapon
+{
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	///  更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+private:
+
+
+
+
+
+public:
+	
+	const WorldTransform& GetWorldTransform() { return worldTransform_; }
+	const Vector3& GetScale() { return worldTransform_.scale_; }
+	const Vector3& GetRotation() { return worldTransform_.rotation_; }
+	const Vector3& GetTranslation() { return worldTransform_.translation_; }
+
+private:
+
+	std::unique_ptr<Object3d> weapon_;
+	WorldTransform worldTransform_;
+
+};
+
